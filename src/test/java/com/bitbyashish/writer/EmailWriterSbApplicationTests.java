443c102bf19782747cddd9c92b7f0e82d2f15cb6
Ashish <ashishtiwari7420@gmail.com>
2025-06-09 14:19:30 +0530

    Initialized Project

---
+++ src/test/java/com/bitbyashish/writer/EmailWriterSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbyashish.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
